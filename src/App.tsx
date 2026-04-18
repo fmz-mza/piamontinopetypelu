a3cfce95e8c1b2ff3bb85ef2f4f61352142a207b
[dyad] <git@dyad.sh>
2026-04-18 20:20:49 -0300

    [dyad] Explicación de entorno virtual y reconstrucción del proyecto. - wrote 8 file(s), added lucide-react, clsx, tailwind-merge package(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import PomodoroTimer from './components/PomodoroTimer';
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
       <PomodoroTimer />
     </div>
   );
